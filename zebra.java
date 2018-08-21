52c63541317c48ec6c7bd458d3ae4d780b20f792
xlabsoft <xlab@ua.fm>
2018-08-21 19:42:00 +0300

    Zebra

---
+++ zebra.java
@@ -0,0 +1,6 @@
+public class Zebra
+{
+    public Zebra(){
+        System.out.println("Marti");
+    }
+}
--- zoo.java
+++ zoo.java
@@ -3,5 +3,6 @@ public class Zoo
 	public static void main(String ... args)
 	{
 		System.out.println("Welcome to ZOO");
+		Zebra zebra = new Zebra();
 	}
 }
